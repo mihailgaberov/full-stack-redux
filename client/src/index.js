da999a2506044b4616e8c82e23b13c05c3fb35db
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-05-28 15:54:50 +0200

    Introduced Redux for the client side. Adding reducers and store.

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router, Route, hashHistory } from 'react-router'
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import reducer from './reducer'
 import App from './components/App'
 import Voting from './components/Voting'
@@ -13,7 +14,7 @@ store.dispatch({
   state: {
     vote: {
       pair: ['Sunshine', '28 Days Later'],
-      tally: {Sunshine: 2}
+      tally: { Sunshine: 2 }
     }
   }
 })
@@ -24,6 +25,8 @@ const routes = <Route component={App}>
 </Route>
 
 ReactDOM.render(
-  <Router history={hashHistory}>{routes}</Router>,
+  <Provider store={store}>
+    <Router history={hashHistory}>{routes}</Router>
+  </Provider>,
   document.getElementById('app')
 )
