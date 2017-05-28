8276771fad7ccb52c30c48391bbfc4b40dd692d0
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-05-28 14:20:12 +0200

    Setuping the client app.

---
+++ client/src/index.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Mihail on 5/28/2017.
+ */
+console.log('I am alive')
---
+++ client/webpack.config.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Mihail on 5/28/2017.
+ */
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+}
