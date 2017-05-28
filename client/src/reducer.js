3ea9045cfaaca6024b905f06f16b9f683cfbd772
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-05-28 15:52:41 +0200

    Introduced Redux for the client side. Adding reducers and store.

---
+++ client/src/reducer.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Mihail on 5/28/2017.
+ */
+import {Map} from 'immutable'
+
+function setState(state, newState) {
+  return state.merge(newState)
+}
+
+export default function(state = Map(), action) {
+  switch (action.type) {
+    case 'SET_STATE':
+      return setState(state, action.state)
+  }
+  return state
+}
