32ef8fc392f19a37c77f2f8afca71754c2e2a8db
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-05-28 13:24:13 +0200

    Voting.

--- src/core.js
+++ src/core.js
@@ -8,9 +8,17 @@ export function setEntries(state, entries) {
 }
 
 export function next(state) {
-  const entries = state.get('entries');
+  const entries = state.get('entries')
   return state.merge({
     vote: Map({ pair: entries.take(2) }),
     entries: entries.skip(2)
   })
+}
+
+export function vote(state, entry) {
+  return state.updateIn(
+    ['vote', 'tally', entry],
+    0,
+    tally => tally + 1
+  )
 }
