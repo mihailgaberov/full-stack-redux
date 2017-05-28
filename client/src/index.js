32578de64d457432f7fd2602e137bbf24e05fc11
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-05-28 16:46:32 +0200

    Create socket object before trying to use it.

--- client/src/index.js
+++ client/src/index.js
@@ -11,6 +11,12 @@ import App from './components/App'
 import { VotingContainer } from './components/Voting'
 import { ResultsContainer } from './components/Results'
 
+const socket = io(`${location.protocol}//${location.hostname}:8090`)
+socket.on('state', state =>
+  store.dispatch(setState(state))
+)
+
+
 const createStoreWithMiddleware = applyMiddleware(
   remoteActionMiddleware(socket)
 )(createStore);
@@ -25,11 +31,6 @@ store.dispatch({
     }
   }
 })
-
-const socket = io(`${location.protocol}//${location.hostname}:8090`)
-socket.on('state', state =>
-  store.dispatch(setState(state))
-)
 
 const routes = <Route component={App}>
   <Route path="/results" component={ResultsContainer}/>
