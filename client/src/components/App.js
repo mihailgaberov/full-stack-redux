6b50e8da6867ba4b304d8e747e300dafb9a33dfe
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-05-28 16:04:06 +0200

    Connected React components with Redux.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,10 @@
 /**
  * Created by Mihail on 5/28/2017.
  */
-import React from 'react'
-import { List, Map } from 'immutable'
-
-const pair = List.of('Trainspotting', '28 Days Later')
-const tally = Map({'Trainspotting': 5, '28 Days Later': 4})
+import React from 'react';
 
 export default React.createClass({
   render: function() {
-    return React.cloneElement(this.props.children, {
-      pair: pair,
-      tally: tally
-    })
+    return this.props.children
   }
 })
