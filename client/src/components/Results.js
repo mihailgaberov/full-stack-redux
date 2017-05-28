e0d0b6401a6059fdec042e76b10fde745510caab
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-05-28 16:03:08 +0200

    Connecting React components with Redux.

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -3,22 +3,23 @@
  */
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
+import {connect} from 'react-redux'
 import Winner from './Winner'
 
-export default React.createClass({
+export const Results = React.createClass({
   mixins: [PureRenderMixin],
-  getPair: function () {
-    return this.props.pair || [];
+  getPair: function() {
+    return this.props.pair || []
   },
-  getVotes: function (entry) {
+  getVotes: function(entry) {
     if (this.props.tally && this.props.tally.has(entry)) {
       return this.props.tally.get(entry)
     }
     return 0
   },
-  render: function () {
+  render: function() {
     return this.props.winner ?
-      <Winner ref="winner" winner={this.props.winner}/> :
+      <Winner ref="winner" winner={this.props.winner} /> :
       <div className="results">
         <div className="tally">
           {this.getPair().map(entry =>
@@ -37,6 +38,16 @@ export default React.createClass({
             Next
           </button>
         </div>
-      </div>;
+      </div>
   }
 })
+
+function mapStateToProps(state) {
+  return {
+    pair: state.getIn(['vote', 'pair']),
+    tally: state.getIn(['vote', 'tally']),
+    winner: state.get('winner')
+  }
+}
+
+export const ResultsContainer = connect(mapStateToProps)(Results)
--- client/test/components/Results_spec.js
+++ client/test/components/Results_spec.js
@@ -9,7 +9,7 @@ import {
   Simulate
 } from 'react-addons-test-utils'
 import { List, Map } from 'immutable'
-import Results from '../../src/components/Results'
+import { Results } from '../../src/components/Results'
 import { expect } from 'chai'
 
 describe('Results', () => {
