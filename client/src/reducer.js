0383f5abe831bcf9767b05b7ee71e6b903198c41
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-05-28 16:10:53 +0200

    Adding handling of voting actions.

--- client/src/reducer.js
+++ client/src/reducer.js
@@ -1,7 +1,7 @@
 /**
  * Created by Mihail on 5/28/2017.
  */
-import {Map} from 'immutable'
+import { List, Map } from 'immutable'
 
 function setState(state, newState) {
   return state.merge(newState)
@@ -17,10 +17,20 @@ function vote(state, entry) {
   }
 }
 
+function resetVote(state) {
+  const hasVoted = state.get('hasVoted');
+  const currentPair = state.getIn(['vote', 'pair'], List());
+  if (hasVoted && !currentPair.includes(hasVoted)) {
+    return state.remove('hasVoted');
+  } else {
+    return state;
+  }
+}
+
 export default function(state = Map(), action) {
   switch (action.type) {
     case 'SET_STATE':
-      return setState(state, action.state)
+      return resetVote(setState(state, action.state))
     case 'VOTE':
       return vote(state, action.entry)
   }
